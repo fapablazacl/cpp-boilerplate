e46f581222aaaa9a8ed8520fb2d0cf94f4ac8caa
Felipe Apablaza <felipe.apablaza@protonmail.com>
2021-09-12 11:59:58 -0300

    fixed CMakeLists integration

--- modules/app/src/main.cpp
+++ modules/app/src/main.cpp
@@ -1,8 +1,11 @@
 
 #include <iostream>
+#include <boost/filesystem.hpp>
+#include <fruit/fruit.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << boost::filesystem::current_path() << std::endl;
 
     return 0;
 }
